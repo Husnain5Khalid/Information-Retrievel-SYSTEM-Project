944d17ce9d67f13e56e5d0f64fde3be6938def2b
Husnain5khalid <husnainkhalidkhan6@gmail.com>
2026-06-25 16:28:10 +0300

    APi_key creatd

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Projects",
+    version = '0.0.0',
+    author = "Khan",
+    author_email= "husnain@gmai.com",
+    packages = find_packages(),
+    install_requires= []
+)
